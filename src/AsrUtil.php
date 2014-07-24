058befdb9ad9af95a8a2af24edd26c9280a3fbd4
csaba.simon <csaba.simon@emarsys.com>
2014-07-24 13:53:46 +0200

    SUITEDEV-1868: aws signed header spike

--- src/AsrUtil.php
+++ src/AsrUtil.php
@@ -2,7 +2,7 @@
 
 class AsrUtil
 {
-    public static function generate($method, $url, $payload, array $headers, array $signedHeaders)
+    public function generate($method, $url, $payload, array $headers, array $signedHeaders)
     {
         $urlParts = parse_url($url);
 
@@ -12,33 +12,33 @@ class AsrUtil
 
         $requestLines = array_merge(
             array(strtoupper($method), $path, $query),
-            self::convertHeaders($headers),
-            array('', self::convertSignedHeaders($signedHeaders), hash('sha256', $payload))
+            $this->convertHeaders($headers),
+            array('', $this->convertSignedHeaders($signedHeaders), hash('sha256', $payload))
         );
 
         return hash('sha256', implode("\n", $requestLines));
     }
 
-    public static function createStringToSign($algo, $date, $credentialScope, $canonicalRequestHash)
+    public function createStringToSign($algorithm, $date, $credentialScope, $canonicalRequestHash)
     {
-        return implode("\n", array($algo, $date, $credentialScope, $canonicalRequestHash));
+        return implode("\n", array($algorithm, $date, $credentialScope, $canonicalRequestHash));
     }
 
-    public static function calculateSigningKey($date, $region, $service, $secretKey)
+    public function calculateSigningKey($date, $region, $service, $secretKey)
     {
         $secret = 'AWS4' . $secretKey;
-        $hashedDate    = self::hmacSha($date, $secret);
-        $hashedRegion  = self::hmacSha($region, $hashedDate);
-        $hashedService = self::hmacSha($service, $hashedRegion);
-        $signing       = self::hmacSha('aws4_request', $hashedService);
+        $hashedDate    = $this->hmacSha($date, $secret);
+        $hashedRegion  = $this->hmacSha($region, $hashedDate);
+        $hashedService = $this->hmacSha($service, $hashedRegion);
+        $signing       = $this->hmacSha('aws4_request', $hashedService);
         return $signing;
     }
 
-    private static function convertHeaders($headers)
+    private function convertHeaders($headers)
     {
         $result = array();
         foreach ($headers as $key => $value) {
-            $result []= strtolower($key) . ':' . self::trimHeaderValue($value);
+            $result []= strtolower($key) . ':' . $this->trimHeaderValue($value);
         }
         return $result;
     }
@@ -47,7 +47,7 @@ class AsrUtil
      * @param $value
      * @return string
      */
-    private static function trimHeaderValue($value)
+    private function trimHeaderValue($value)
     {
         return trim($value);
     }
@@ -56,7 +56,7 @@ class AsrUtil
      * @param array $signedHeaders
      * @return string
      */
-    private static function convertSignedHeaders(array $signedHeaders)
+    private function convertSignedHeaders(array $signedHeaders)
     {
         return implode(';', array_map('strtolower', $signedHeaders));
     }
@@ -67,7 +67,7 @@ class AsrUtil
      * @param $raw
      * @return string
      */
-    private static function hmacSha($data, $key, $raw = true)
+    private function hmacSha($data, $key, $raw = true)
     {
         return hash_hmac('sha256', $data, $key, $raw);
     }
--- test/unit/AsrUtilTest.php
+++ test/unit/AsrUtilTest.php
@@ -17,15 +17,8 @@ class AsrUtilTest extends PHPUnit_Framework_TestCase
      */
     public function itShouldGenerateSignedRequestHeader()
     {
-        $headers = array(
-            'Content-Type' => 'application/x-www-form-urlencoded; charset=utf-8',
-            'Host' => 'iam.amazonaws.com',
-            'X-Amz-Date' => '20110909T233600Z',
-        );
-        $signedHeaders = array_keys($headers);
-        $payload = 'Action=ListUsers&Version=2010-05-08';
-        $result = $this->util->generate('POST', 'http://iam.amazonaws.com/', $payload, $headers, $signedHeaders);
-        $this->assertEquals('3511de7e95d28ecd39e9513b642aee07e54f4941150d8df8bf94b328ef7e55e2', $result);
+        $result = $this->util->generate('POST', 'http://iam.amazonaws.com/', $this->payload(), $this->headers(), array_keys($this->headers()));
+        $this->assertEquals($this->canonicalHash(), $result);
     }
 
     /**
@@ -46,7 +39,8 @@ class AsrUtilTest extends PHPUnit_Framework_TestCase
      */
     public function itShouldCalculateSigningKey()
     {
-        $result = $this->util->calculateSigningKey('20120215', 'us-east-1', 'iam', 'wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY');
+        $date = '20120215';
+        $result = $this->util->calculateSigningKey($date, $this->region(), $this->service(), $this->secretKey());
         $this->assertEquals($this->signingKey(), bin2hex($result));
     }
 
@@ -55,8 +49,8 @@ class AsrUtilTest extends PHPUnit_Framework_TestCase
      */
     public function itShouldSign()
     {
-        $sign = $this->util->signRequest($this->stringToSign(), '20110909', 'us-east-1', 'iam', 'wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY');
-        $this->assertEquals('ced6826de92d2bdeed8f846f0bf508e8559e98e4b0199114b84c54174deb456c', $sign);
+        $sign = $this->util->signRequest($this->stringToSign(), $this->date(), $this->region(), $this->service(), $this->secretKey());
+        $this->assertEquals($this->signedRequest(), $sign);
     }
 
     /**
@@ -64,10 +58,9 @@ class AsrUtilTest extends PHPUnit_Framework_TestCase
      */
     private function stringToSign()
     {
-        return "AWS4-HMAC-SHA256
-20110909T233600Z
-20110909/us-east-1/iam/aws4_request
-3511de7e95d28ecd39e9513b642aee07e54f4941150d8df8bf94b328ef7e55e2";
+        return implode("\n", array(
+            'AWS4-HMAC-SHA256', '20110909T233600Z', '20110909/us-east-1/iam/aws4_request', $this->canonicalHash()
+        ));
     }
 
     /**
@@ -84,5 +77,65 @@ class AsrUtilTest extends PHPUnit_Framework_TestCase
     private function signingKey()
     {
         return 'f4780e2d9f65fa895f9c67b32ce1baf0b0d8a43505a000a1a9e090d414db404d';
+    }
+
+    /**
+     * @return string
+     */
+    private function signedRequest()
+    {
+        return 'ced6826de92d2bdeed8f846f0bf508e8559e98e4b0199114b84c54174deb456c';
+    }
+
+    /**
+     * @return string
+     */
+    private function secretKey()
+    {
+        return 'wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY';
+    }
+
+    /**
+     * @return string
+     */
+    private function service()
+    {
+        return 'iam';
+    }
+
+    /**
+     * @return string
+     */
+    private function region()
+    {
+        return 'us-east-1';
+    }
+
+    /**
+     * @return string
+     */
+    private function date()
+    {
+        return '20110909';
+    }
+
+    /**
+     * @return array
+     */
+    private function headers()
+    {
+        return array(
+            'Content-Type' => 'application/x-www-form-urlencoded; charset=utf-8',
+            'Host' => 'iam.amazonaws.com',
+            'X-Amz-Date' => '20110909T233600Z',
+        );
+    }
+
+    /**
+     * @return string
+     */
+    private function payload()
+    {
+        return 'Action=ListUsers&Version=2010-05-08';
     }
 }
