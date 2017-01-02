7291498ce4f849a168a630c4ebad551207eef132
Attila Droszler <adroszler@emarsys.com>
2017-01-02 15:28:18 +0000

    add class EscherProvider

--- src/Escher.php
+++ src/Escher.php
@@ -928,3 +928,53 @@ class EscherUtils
         return version_compare(PHP_VERSION, '5.3.0') !== -1;
     }
 }
+
+
+class EscherProvider
+{
+    private $credentialScope;
+    private $escherKey;
+    private $escherSecret;
+    private $keyDB;
+
+    public function __construct($credentialScope, $escherKey, $escherSecret, $keyDB)
+    {
+        $this->credentialScope = $credentialScope;
+        $this->escherKey = $escherKey;
+        $this->escherSecret = $escherSecret;
+        $this->keyDB = $keyDB;
+    }
+
+    public function createEscher() : Escher
+    {
+        return Escher::create($this->credentialScope)
+            ->setAlgoPrefix('EMS')
+            ->setVendorKey('EMS')
+            ->setAuthHeaderKey('X-Ems-Auth')
+            ->setDateHeaderKey('X-Ems-Date');
+    }
+
+    /**
+     * @return string
+     */
+    public function getEscherKey() : string
+    {
+        return $this->escherKey;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEscherSecret() : string
+    {
+        return $this->escherSecret;
+    }
+
+    /**
+     * @return \ArrayAccess|array
+     */
+    public function getKeyDB()
+    {
+        return $this->keyDB;
+    }
+}
