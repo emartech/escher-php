40d7e4abeac45317f3c8353b214bafdb3d170168
David Szigecsan <david.szigecsan@emarsys.com>
2020-08-30 11:28:40 +0200

    Merge str_replace functions to improve performance

--- src/Escher/RequestCanonicalizer.php
+++ src/Escher/RequestCanonicalizer.php
@@ -59,9 +59,7 @@ class RequestCanonicalizer
         }
 
         $path = implode('/', $path);
-        $path = str_replace('./', '', $path);
-
-        $path = str_replace('//', '/', $path);
+        $path = str_replace(array('./', '//'), array('', '/'), $path);
 
         if (empty($path)) {
             return '/';
